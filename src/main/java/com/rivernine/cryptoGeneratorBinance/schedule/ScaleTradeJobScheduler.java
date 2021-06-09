d09503d98b535f9ccc532977987a8743bd879ed1
rivernine <rivernine@github.com>
2021-06-09 11:13:42 +0900

    save

--- src/main/java/com/rivernine/cryptoGeneratorBinance/schedule/ScaleTradeJobScheduler.java
+++ src/main/java/com/rivernine/cryptoGeneratorBinance/schedule/ScaleTradeJobScheduler.java
@@ -1,5 +1,8 @@
 package com.rivernine.cryptoGeneratorBinance.schedule;
 
+import com.rivernine.cryptoGeneratorBinance.client.SyncRequestClient;
+import com.rivernine.cryptoGeneratorBinance.client.model.enums.CandlestickInterval;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,5 +16,7 @@ public class ScaleTradeJobScheduler {
   @Scheduled(fixedDelay = 1000)
   public void runGetMultipleCandlesJob() {
     log.info("Get Candles Job");
+    log.info(SyncRequestClient.create().getCandlestick("BTCUSDT", CandlestickInterval.FIVE_MINUTES, 1499865549590L, 1599865549590L, 1).toString());
+
   }
 }
