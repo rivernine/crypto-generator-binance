5e7e27e50490a311fc3a2267f5e55fb4127bbb97
rivernine <rivernine@github.com>
2021-06-11 14:23:17 +0900

    fixed untradcked files

--- src/test/java/com/rivernine/cryptoGeneratorBinance/client/constants/PrivateConfig.java
+++
@@ -1,8 +0,0 @@
-package com.rivernine.cryptoGeneratorBinance.client.constants;
-
-public class PrivateConfig {
-
-    public static final String API_KEY = "XXXXXX";
-    public static final String SECRET_KEY = "XXXXXX";
-
-}
